1d1acaf6e6923d1eba78e29f34fb3061edc600bc
Sam Cannon <samcannon10@ksu.edu>
2019-10-14 10:59:02 -0500

    Added dynamic addition and removal of buttons to drink selection page

--- PointOfSale/DrinkSelection.xaml.cs
+++ PointOfSale/DrinkSelection.xaml.cs
@@ -30,6 +30,12 @@ namespace PointOfSale
         public DrinkSelection()
         {
             InitializeComponent();
+            LemonButton.Visibility = Visibility.Hidden;
+            LemonButton.Visibility = Visibility.Collapsed;
+            DecafButton.Visibility = Visibility.Hidden;
+            DecafButton.Visibility = Visibility.Collapsed;
+            SweetButton.Visibility = Visibility.Hidden;
+            SweetButton.Visibility = Visibility.Collapsed;
         }
 
         /// <summary>
@@ -40,6 +46,58 @@ namespace PointOfSale
         public void ChooseFlavor(object sender, RoutedEventArgs args)
         {
             NavigationService.Navigate(new FlavorSelection());
+        }
+
+        public void WaterClick(object sender, RoutedEventArgs args)
+        {
+            LemonButton.Visibility = Visibility.Visible;
+            IceButton.Visibility = Visibility.Visible;
+
+            FlavorButton.Visibility = Visibility.Hidden;
+            FlavorButton.Visibility = Visibility.Collapsed;
+            SweetButton.Visibility = Visibility.Hidden;
+            SweetButton.Visibility = Visibility.Collapsed;
+            DecafButton.Visibility = Visibility.Hidden;
+            DecafButton.Visibility = Visibility.Collapsed;
+        }
+
+        public void SodasaurusClick(object sender, RoutedEventArgs args)
+        {
+            IceButton.Visibility = Visibility.Visible;
+            FlavorButton.Visibility = Visibility.Visible;
+
+            SweetButton.Visibility = Visibility.Hidden;
+            SweetButton.Visibility = Visibility.Collapsed;
+            DecafButton.Visibility = Visibility.Hidden;
+            DecafButton.Visibility = Visibility.Collapsed;
+            LemonButton.Visibility = Visibility.Hidden;
+            LemonButton.Visibility = Visibility.Collapsed;
+        }
+
+        public void JavaClick(object sender, RoutedEventArgs args)
+        {
+            DecafButton.Visibility = Visibility.Visible;
+
+            LemonButton.Visibility = Visibility.Hidden;
+            LemonButton.Visibility = Visibility.Collapsed;
+            SweetButton.Visibility = Visibility.Hidden;
+            SweetButton.Visibility = Visibility.Collapsed;
+            FlavorButton.Visibility = Visibility.Hidden;
+            FlavorButton.Visibility = Visibility.Collapsed;
+            IceButton.Visibility = Visibility.Hidden;
+            IceButton.Visibility = Visibility.Collapsed;
+        }
+
+        public void TyrannoteaClick(object sender, RoutedEventArgs args)
+        {
+            LemonButton.Visibility = Visibility.Visible;
+            SweetButton.Visibility = Visibility.Visible;
+            IceButton.Visibility = Visibility.Visible;
+
+            DecafButton.Visibility = Visibility.Hidden;
+            DecafButton.Visibility = Visibility.Collapsed;
+            FlavorButton.Visibility = Visibility.Hidden;
+            FlavorButton.Visibility = Visibility.Collapsed;
         }
     }
 }
