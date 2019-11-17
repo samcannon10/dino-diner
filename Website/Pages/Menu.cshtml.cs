d61aebc00d0c68e959f5b1c88093ac24d9244ed2
Sam Cannon <samcannon10@ksu.edu>
2019-11-17 15:43:15 -0600

    Created Dino-Diner sites

---
+++ Website/Pages/Menu.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using DinoDiner.Menu;
+
+namespace Website.Pages
+{
+    public class MenuModel : PageModel
+    {
+        public Menu menu { get; } = new Menu();
+
+        public void OnGet()
+        {
+            
+        }
+    }
+}
